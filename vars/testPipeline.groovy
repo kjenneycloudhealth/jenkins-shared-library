e763a5a5d762de67d928ad9cad1b30e02e57881e
Ken Jenney <kjenney@cloudhealthtech.com>
2021-03-05 10:26:01 -0500

    Replace steps with command

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -2,9 +2,7 @@ def call(int buildTimeout) {
     node() {
       stage('Prepare') {
         timeout(time: buildTimeout, unit: 'MINUTES') {
-          steps {
-            echo "test this"
-          }
+          echo "test this"
         }
       }
     }
